cba34cd8ead35451947cb577990685466c39a934
SoloVova <vsolo0@gmail.com>
2022-01-03 13:00:47 +0200

    1.01 Versioning work

--- src/http-common.ts
+++ src/http-common.ts
@@ -1,7 +1,7 @@
 ﻿import axios from "axios";
 
 const http = axios.create({
-    baseURL: "https://localhost:5001/api",
+    baseURL: "https://localhost:5001/api/1.0",
     headers: {
         "Content-type": "application/json"
     }
